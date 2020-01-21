876c22331722339898cb85b5f056303f3e65dbf0
Admin <pranali.lembhe77@gmail.com>
2020-01-21 15:03:20 +0530

    ADDED:Daily Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+readonly WAGE_FOR_HOUR=20
+readonly FULL_DAY_HOUR=8
 isPresent=1
+isAbsent=0
 echo "Welcome to Employee Wage"
 
 check=$((RANDOM%2))
 if (($check == $isPresent ))
 then
 		echo "Employee Present"
+		dailyWage=$(( $WAGE_FOR_HOUR * $FULL_DAY_HOUR ))
+		echo "Daily Employee Wage: $dailyWage"
 else
 		echo "Employee Absent"
 fi
