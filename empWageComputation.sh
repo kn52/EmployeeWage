7651d4f3174f64f02bf78511403a90cf63ed32e0
Admin <pranali.lembhe77@gmail.com>
2020-01-21 14:37:46 +0530

    ADDED: Employee Attandance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+isPresent=1
 echo "Welcome to Employee Wage"
+
+check=$((RANDOM%2))
+if (($check == $isPresent ))
+then
+		echo "Employee Present"
+else
+		echo "Employee Absent"
+fi
+
