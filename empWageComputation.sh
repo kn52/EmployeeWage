2b3217658e02a9e33540847f5a047927e39ed213
kn52 <ashish52922@gmail.com>
2020-01-21 17:45:54 +0530

    MODIFIED:Monthly Wage Using While

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,12 +4,15 @@ readonly WAGE_FOR_HOUR=20
 readonly FULL_DAY_HOUR=8
 readonly PART_TIME_HOUR=4
 readonly TOTAL_DAYS=20
+readonly TOTAL_HOURS=100
+
 totalhrs=0
+days=0
 echo "Welcome to Employee Wage"
 
-for (( c=1; c<=$TOTAL_DAYS; c++ ))
+while (( $totalhrs < $TOTAL_HOURS || $days < $TOTAL_DAYS ))
 do
-
+	((days++))
 	check=$((RANDOM%3))
 
 	case $check in
